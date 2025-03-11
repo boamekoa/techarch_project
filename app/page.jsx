f85905879a1bbaa5d4940c3908f1d2c56ad0e507
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2025-03-11 15:32:07 +0000

    project starter
    This is the starter project setup

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Home",
+  description: "A full-stack techarch application for clients",
+};
+
+const Home = () => {
+  return <main className="text-blue-800 text-4xl">Home</main>;
+};
+
+export default Home;
